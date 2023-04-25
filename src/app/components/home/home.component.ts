1f21753b87348e941da3976c34cfe84562b96527
Gabriela Sanchez <74748682+gabrielasan20117@users.noreply.github.com>
2023-04-24 21:22:05 -0600

    OPERADOR MAP PART 1

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -15,8 +15,7 @@ export class HomeComponent implements OnInit {
 
     this.spotify.getNetRealeases()
         .subscribe((data: any) => {
-          console.log(data.playlists.items);
-          this.nuevas_canciones = data.playlists.items
+          this.nuevas_canciones = data
         });
    }
 
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,12 +11,15 @@ export class SpotifyService {
    }
   
   getNetRealeases(){
-  var valores = 'Bearer BQAZiFFXBqzNLnTUbwZoNJLsXft1xB051IDnpTUWBhUWtBVLxhgNFDc48CAFeU0RkLot05P7SWHIOzPePbQ92b9L0OVnHME4WRTHSWtQCJcFzWTldBa4'
+  var valores = 'Bearer BQDZMk3cCnTn1c0WUBqZj-uTSWvj6tdC8JIMWs47sy15zgqf1Z5L4KPu-1tQE1Hin5qQ1HqWFNZJK2fqmG8Ib75u9sm1U1JP27UMB1TVrvs_WP5r28h_'
     const headers = new HttpHeaders({
       'Authorization': valores
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/featured-playlists', {headers});
+    return this.http.get('https://api.spotify.com/v1/browse/featured-playlists', {headers})
+                .pipe( map( (data: any) => {
+                  return data['playlists'].items;
+                }))
   }
 
   getArtista(termino : string){
